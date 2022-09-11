cb18218eb3d6bae50c10c21eb4c255d5e02035ed
Mika Viljanen <mika.viljanen@wunderdog.fi>
2022-09-11 17:52:31 +0300

    Add player movement with cursor keys

--- src/scenes/gameScene.ts
+++ src/scenes/gameScene.ts
@@ -2,6 +2,9 @@ import { TILE_SIZE } from "../index";
 
 export class GameScene extends Phaser.Scene {
   player!: Phaser.GameObjects.GameObject;
+  cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
+
+  #playerMoving = false;
 
   constructor() {
     super({key: "GameScene"});
@@ -12,6 +15,25 @@ export class GameScene extends Phaser.Scene {
   }
 
   create() {
-    this.add.sprite(TILE_SIZE * 10, TILE_SIZE * 10, "player");
+    this.player = this.add.sprite(TILE_SIZE * 10.5, TILE_SIZE * 10.5, "player");
+    this.cursors = this.input.keyboard.createCursorKeys();
+  }
+
+  update() {
+    if (!this.#playerMoving) {
+      if (this.cursors.left!.isDown) {
+        this.player.x -= TILE_SIZE;
+      } else if (this.cursors.right!.isDown) {
+        this.player.x += TILE_SIZE;
+      }
+
+      if (this.cursors.up!.isDown) {
+        this.player.y -= TILE_SIZE;
+      } else if (this.cursors.down!.isDown) {
+        this.player.y += TILE_SIZE;
+      }
+    }
+
+    this.#playerMoving = (this.cursors.left!.isDown || this.cursors.right!.isDown || this.cursors.up!.isDown || this.cursors.down!.isDown);
   }
 }

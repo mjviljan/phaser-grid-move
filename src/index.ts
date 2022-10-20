57998d67cd342246cfb35a791609cce83e988782
Mika Viljanen <mika.viljanen@wunderdog.fi>
2022-10-20 15:59:55 +0300

    Update graphics (with self-made ones), detect blocked squares when moving

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { GameScene } from "./scenes/gameScene";
 
-export const TILE_SIZE = 64;
+export const TILE_SIZE = 32;
 
 export function startGame() {
   const config: Phaser.Types.Core.GameConfig = {
@@ -8,8 +8,8 @@ export function startGame() {
     scale: {
       mode: Phaser.Scale.FIT,
       autoCenter: Phaser.Scale.CENTER_BOTH,
-      width: TILE_SIZE * 12,
-      height: TILE_SIZE * 12,
+      width: TILE_SIZE * 24,
+      height: TILE_SIZE * 24,
     },
     parent: "game",
     scene: [GameScene],
--- src/scenes/gameScene.ts
+++ src/scenes/gameScene.ts
@@ -1,9 +1,23 @@
 import { TILE_SIZE } from "../index";
 
+type DirectionDelta = {
+  x: number,
+  y: number
+}
+
+const Direction = {
+  Up: {x: 0, y: -1},
+  Right: {x: 1, y: 0},
+  Down: {x: 0, y: 1},
+  Left: {x: -1, y: 0}
+}
+
 export class GameScene extends Phaser.Scene {
   player!: Phaser.GameObjects.Sprite;
+  obstacleLayer!: Phaser.Tilemaps.TilemapLayer;
   cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
 
+  #playerPos = {x: 3, y: 3};
   #playerMoving = false;
 
   constructor() {
@@ -11,27 +25,48 @@ export class GameScene extends Phaser.Scene {
   }
 
   preload() {
-    this.load.image("player", "assets/images/player_05.png");
+    this.load.image("bg-tiles", "assets/tilesets/world-tiles.png");
+    this.load.image("player", "assets/images/character.png");
+    this.load.tilemapTiledJSON("first-screen", `assets/maps/test-world.json`);
   }
 
   create() {
-    this.player = this.add.sprite(TILE_SIZE * 1.5, TILE_SIZE * 1.5, "player");
+    const map = this.make.tilemap({key: "first-screen"});
+    const tiles = map.addTilesetImage("bg-tiles");
+    map.createLayer(0, tiles, 0, 0);
+    this.obstacleLayer = map.createLayer(1, tiles, 0, 0);
+
+    this.player = this.add.sprite(TILE_SIZE * this.#playerPos.x, TILE_SIZE * this.#playerPos.y, "player");
+    this.player.setDisplayOrigin(0, 0);
+
     this.cursors = this.input.keyboard.createCursorKeys();
   }
 
   update() {
     if (!this.#playerMoving) {
       if (this.cursors.left!.isDown) {
-        this.player.x -= TILE_SIZE;
+        this.tryToMoveTo(Direction.Left)
       } else if (this.cursors.right!.isDown) {
-        this.player.x += TILE_SIZE;
+        this.tryToMoveTo(Direction.Right)
       } else if (this.cursors.up!.isDown) {
-        this.player.y -= TILE_SIZE;
+        this.tryToMoveTo(Direction.Up)
       } else if (this.cursors.down!.isDown) {
-        this.player.y += TILE_SIZE;
+        this.tryToMoveTo(Direction.Down)
       }
     }
 
     this.#playerMoving = (this.cursors.left!.isDown || this.cursors.right!.isDown || this.cursors.up!.isDown || this.cursors.down!.isDown);
   }
+
+  tryToMoveTo(dir: DirectionDelta) {
+    if (this.isDirectionBlocked(dir)) return;
+
+    this.#playerPos.x += dir.x;
+    this.#playerPos.y += dir.y;
+
+    this.player.x = this.#playerPos.x * TILE_SIZE;
+    this.player.y = this.#playerPos.y * TILE_SIZE;
+  }
+
+  isDirectionBlocked = (dir: DirectionDelta) => !!this.obstacleLayer.getTileAt(this.#playerPos.x + dir.x, this.#playerPos.y + dir.y);
 }
